18c9500f3e5a558b749635f0291ec6f15718d5b2
jcha25 <justicech25@gmail.com>
2023-08-31 13:30:43 -0500

    setup post route for creating an ote

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,16 @@
 const router = require("express").Router()
+const { v4: uuidv4 } = require("uuid")
 const { readFromFile, readAndAppend, readAndDelete } = require("../utils/helpers")
-
 
 router.get("/notes", (req, res) => {
     readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+})
+
+router.post("/notes", (req, res) => {
+    const note = req.body;
+    note.id = uuidv4();
+    readAndAppend(note, "./db/db.json")
+    res.json(note)
 })
 
 module.exports = router;
