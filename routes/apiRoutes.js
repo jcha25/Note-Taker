c28d50fd127272f241c60b790f3ccf8499397b34
jcha25 <justicech25@gmail.com>
2023-08-31 13:26:15 -0500

    setup get route for getting api data from db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,4 +2,8 @@ const router = require("express").Router()
 const { readFromFile, readAndAppend, readAndDelete } = require("../utils/helpers")
 
 
+router.get("/notes", (req, res) => {
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+})
+
 module.exports = router;
